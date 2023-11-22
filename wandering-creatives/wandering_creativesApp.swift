242f42db200d90b13f850205c4fd487c7a19678d
Rayan Popat <rayan.popat@kcl.ac.uk>
2023-11-22 15:20:45 +0000

    Initial Commit

---
+++ wandering-creatives/wandering_creativesApp.swift
@@ -0,0 +1,17 @@
+//
+//  wandering_creativesApp.swift
+//  wandering-creatives
+//
+//  Created by Rayan Popat on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct wandering_creativesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
